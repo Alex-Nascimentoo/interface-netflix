371d490d912533182e80c3ca45d0bd5f3436f20b
Alex Nascimento <alexdavi150@gmail.com>
2020-11-09 10:49:33 -0300

    Esta é a primeira 'fase' do projeto, digamos assim, com o tempo aprimorarei o projeto, se você quiser me ajudar será muito bem vindo :)

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.populares-netflix-imagens').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    variableWidth: true
+});
+
+$('.originais-imagens').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    variableWidth: true
+});
+
+$('.comedia-imagens').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    variableWidth: true
+});
+
+$('.sitcom-imagens').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    variableWidth: true
+});
